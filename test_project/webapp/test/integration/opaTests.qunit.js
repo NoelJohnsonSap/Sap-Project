aa68556c64bd69095b72df0dc3087e6f2ecd0397
Noel Johnson <noel.johnson@applexus.com>
2024-08-28 10:30:39 +0000

    first commit

---
+++ test_project/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/test/testproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ test_project/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comtest/test_project/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
